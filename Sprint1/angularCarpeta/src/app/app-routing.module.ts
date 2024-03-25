521c53754db3d58fb534d17cecef7f8d9b5ba320
Stevee04 <104826601+Stevee04@users.noreply.github.com>
2024-03-25 14:13:05 +0100

    modificaicon routing, registro

--- Sprint1/angularCarpeta/src/app/app-routing.module.ts
+++ Sprint1/angularCarpeta/src/app/app-routing.module.ts
@@ -1,63 +1,36 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
-// // Importamos las rutas de nuestros componentes //
-// import { LoginComponent } from './login/login.component';
-// import { RegistroComponent } from './registro/registro.component';
-
-// const routes: Routes = [
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//     //canActivate: const [AngularFireAuthGuard]
-//   },
-
-//   { 
-//     path: 'registro',
-//     component: RegistroComponent
-//     //canActivate: const [AngularFireAuthGuard]
-//   },
-
-//   {
-//     path: 'home',
-//     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
-//     canActivate: const [AngularFireAuthGuard]
-//   },
-//   {
-//     path: '**',
-//     redirectTo: 'login',
-//   },
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule],
-// })
-// export class AppRoutingModule {}
-
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AngularFireAuthGuard } from '@angular/fire/auth'; // Importa AngularFireAuthGuard desde AngularFire
-
-// Importa tus componentes
+// import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+// Importamos las rutas de nuestros componentes //
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './main/home/home.component';
-import { MainComponent } from './main/main.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }, // Ruta pública
-  { 
-    path: 'home',
-    component: HomeComponent,
-    // canActivate: [AngularFireAuthGuard], // Protege esta ruta con el guardia de ruta de AngularFire
+  {
+    path: 'login',
+    component: LoginComponent,
+    //canActivate: const [AngularFireAuthGuard]
   },
-  { path: 'other', component: MainComponent }, // Ruta pública
-  { path: '**', redirectTo: 'login' }, // Redirige a login para cualquier otra ruta no definida
+
+  { 
+    path: 'registro',
+    component: RegistroComponent
+    //canActivate: const [AngularFireAuthGuard]
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+    // canActivate: const [AngularFireAuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
