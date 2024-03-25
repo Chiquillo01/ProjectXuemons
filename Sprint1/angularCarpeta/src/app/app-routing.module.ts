00ac88e4fd8caa299e89e4c793db553e4b5fc7f2
Stevee04 <104826601+Stevee04@users.noreply.github.com>
2024-03-25 12:18:57 +0100

    1234

--- Sprint1/angularCarpeta/src/app/app-routing.module.ts
+++ Sprint1/angularCarpeta/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Importamos las rutas de nuestros componentes //
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {
@@ -9,9 +10,16 @@ const routes: Routes = [
     component: LoginComponent,
     //canActivate: const [AngularFireAuthGuard]
   },
+
+  { 
+    path: 'registro',
+    component: RegistroComponent
+    //canActivate: const [AngularFireAuthGuard]
+  },
+
   {
     path: 'home',
-    loadChildren: () => import('./main//main.module').then((m) => m.MainModule),
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
     //canActivate: const [AngularFireAuthGuard]
   },
   {
